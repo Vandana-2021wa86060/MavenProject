f58cfb669245417d2576c6d27d224de9383d8357
Vandana-2021wa86060 <2021wa86060@wilp.bits-pilani.ac.in>
2025-07-14 22:30:33 +0530

    Java

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+	System.out.println("Hello Jenkins CLI");
+	    System.out.println("Hello Jenkins CLI");
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
